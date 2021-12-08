8a3938e91bb45996ed34e817b74e231f9fdbb2e1
Patrick Mooney <pmooney@pfmooney.com>
2021-12-08 11:34:10 -0600

    Prep for exposition

--- check_rtime/src/exceptions.rs
+++ check_rtime/src/exceptions.rs
@@ -1,3 +1,16 @@
+// This file and its contents are supplied under the terms of the
+// Common Development and Distribution License ("CDDL"), version 1.0.
+// You may only use this file in accordance with the terms of version
+// 1.0 of the CDDL.
+//
+// A full copy of the text of the CDDL should have accompanied this
+// source.  A copy of the CDDL is also available via the Internet at
+// http://www.illumos.org/license/CDDL.
+
+//
+// Copyright 2021 Oxide Computer Company
+//
+
 use std::collections::hash_map::HashMap;
 use std::io::Result;
 use std::io::{BufRead, BufReader, Read};
